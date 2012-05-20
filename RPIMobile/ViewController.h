57428a1826a9aef26bab0a266225b9878e71c8cc
Stephen Silber <stephensilber@gmail.com>
2012-05-20 00:45:12 -0400

    Initial Commit

---
+++ RPIMobile/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RPIMobile
+//
+//  Created by Stephen Silber on 5/20/12.
+//  Copyright (c) 2012 Rensselaer Polytechnic Institute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ RPIMobile/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RPIMobile
+//
+//  Created by Stephen Silber on 5/20/12.
+//  Copyright (c) 2012 Rensselaer Polytechnic Institute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

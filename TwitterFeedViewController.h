53f21c66a72c36ecb18340a5ed456a3ab7f1246a
Stephen Silber <stephensilber@gmail.com>
2012-07-02 15:22:05 -0400

    General Fixes
    YouTube thumbnails fixed, new weather icons that are local, twitter is
    a work in progress, some directory bugs fixed

---
+++ TwitterFeedViewController.h
@@ -0,0 +1,13 @@
+//
+//  TwitterFeedViewController.h
+//  RPIMobile
+//
+//  Created by Stephen Silber on 7/1/12.
+//  Copyright (c) 2012 Rensselaer Polytechnic Institute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TwitterFeedViewController : UITableViewController
+
+@end
